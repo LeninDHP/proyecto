dda3f342d33f9383551cf9fd38d58862be144239
LeninDHP <donlino_9012@hotmail.com>
2015-07-28 16:35:17 -0500

    subido

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
 
-<?php
-  session_start();
-  if(isset($_SESSION['user'])){
-  echo '<script> window.location="administrativo.php"; </script>';
-  }
-?>
 
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
